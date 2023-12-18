0d57d71b3cd204db1dd7d6ab18c7765f28b6e54d
Gilmore <144559541+gilmore-ec@users.noreply.github.com>
2023-12-18 07:03:48 -0500

    Made extend JFrame

--- src/TimeCalculator.java
+++ src/TimeCalculator.java
@@ -2,26 +2,31 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.*;
+import java.util.Date;
 
 public class TimeCalculator extends Calculator {
-    private JTextField start;
-    private JTextField end;
+    private final JTextField start;
+    private boolean startMorning;
+    private final JTextField end;
+    private boolean endMorning;
     TimeCalculator() {
         //set up the displays
         start = new JTextField();
-        start.setBounds(50,25,100,50);
+        start.setBounds(50,35,40,30);
         start.setEditable(false);
-        end = calculations;
-        end.setBounds(202,25,100,50);
+        end = new JTextField();
+        end.setEditable(false);
+        end.setFocusable(false);
+        end.setBounds(285,35,40,30);
 
-        String sysTime = getSystemTime();
+        getSystemTime();
 
         //add labels to the input displays
-        JLabel startLabel = new JLabel("Start");
+        JLabel startLabel = new JLabel("Start:");
         startLabel.setBounds(50,10,50,15);
         startLabel.setFocusable(false);
-        JLabel endLabel = new JLabel("End");
-        endLabel.setBounds(202,10,50,15);
+        JLabel endLabel = new JLabel("End:");
+        endLabel.setBounds(285,10,50,15);
         endLabel.setFocusable(false);
         JLabel startPartOfDay = new JLabel("AM");
         startPartOfDay.setBounds(150,45,25,15);
@@ -32,6 +37,7 @@ public class TimeCalculator extends Calculator {
 
         //add the new Display to the frame
         add(start);
+        add(end);
         add(startLabel);
         add(endLabel);
         add(startPartOfDay);
@@ -39,34 +45,35 @@ public class TimeCalculator extends Calculator {
     }
 
     /**
-     *
+     * retrieves system time and sets the value for the start
      */
-    private String getSystemTime() {
-        try {
-            InetAddress localhost = InetAddress.getLocalHost();
-            String url = "https://timeapi.io/api/Time/current/ip=" + localhost.toString();
-            URL api = new URL(url);
-            HttpURLConnection c = (HttpURLConnection) api.openConnection();
-            c.setRequestMethod("GET");
-            if (c.getResponseCode() == 200) {
-                System.out.println("success");
-                return "success";
-            }
-            else {
-                System.out.println(c.getResponseCode());
-                System.out.println(url);
-                return "fail";
-            }
+    private void getSystemTime() {
+        Date date = new Date();
+        String startTime = date.toString().substring(11,16);
+        int hours = Integer.parseInt(startTime.substring(0,2));
+        if (hours > 12) {
+            startMorning = true;
         }
-        catch (IOException e) {
-            return "fail";
-        }
+
+        hours = (startMorning? hours:hours%12);
+
+        startTime = String.format("%02d%s",hours,startTime.substring(2));
+
+        start.setText(startTime);
+
+        //testing purposes
+        System.out.println("Current Time: " + startTime + (startMorning? " AM":" PM"));
+
     }
     /**
      * Sets the format to the calculations JTextField
      */
     @Override
-    public void formatDisplay() {}
+    public void formatDisplay() {
+        calculations.setFocusable(true);
+        calculations.setEditable(true);
+        calculations.setBounds(100,35,80,30);
+    }
 
     /**
      * Adds class-specific JComponents to the JFrame
@@ -91,6 +98,89 @@ public class TimeCalculator extends Calculator {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        //todo
+        for (int i = 0; i<10 ; i++) {
+            if (e.getSource() == buttons[i]) {
+                calculations.setText(calculations.getText()+i);
+            }
+        }
+        if (e.getSource() == decB) {
+            calculations.setText(calculations.getText()+(calculations.getText().contains(".")? "": "."));
+        }
+        if (e.getSource() == delB && !calculations.getText().isEmpty()) {
+            calculations.setText(calculations.getText().substring(0,calculations.getText().length()-1));
+        }
+        if (e.getSource() == clrB) {
+            if (!calculations.getText().isEmpty()) {
+                calculations.setText("");
+            } else num1 = 0;
+        }
+        if (e.getSource() == addB && !("" + num1).isEmpty()) {
+            operator = '+';
+            num1 = Double.parseDouble((negative? "-":"") + calculations.getText());
+            calculations.setText("");
+        }
+        if (e.getSource() == subB && calculations.getText().isEmpty()) {
+            calculations.setText("-");
+        }
+        else if (e.getSource() == subB && !("" + num1).isEmpty()) {
+            operator = '-';
+            num1 = Double.parseDouble((negative? "-":"") + calculations.getText());
+            calculations.setText("");
+        }
+        else if (e.getSource() == subB && ("" + num1).isEmpty()) {
+            negative = true;
+        }
+        if (e.getSource() == altB && !("" + num1).isEmpty()) {
+            operator = '/';
+            num1 = Double.parseDouble((negative? "-":"") + calculations.getText());
+            calculations.setText("");
+        }
+        if (e.getSource() == altA && !("" + num1).isEmpty()) {
+            operator = '*';
+            num1 = Double.parseDouble((negative? "-":"") + calculations.getText());
+            calculations.setText("");
+        }
+        if (e.getSource() == equalsB) {
+            if (!calculations.getText().isEmpty()) {
+                num2 = Double.parseDouble(calculations.getText());
+                if (!Double.toString(num1).isEmpty() && !Double.toString(num2).isEmpty()) {
+                    switch (operator) {
+                        case '+' -> {
+                            result = num1 + num2;
+                            calculations.setText("" + result);
+                        }
+                        case '-' -> {
+                            result = num1 - num2;
+                            calculations.setText("" + result);
+                        }
+                        case '*' -> {
+                            result = num1 * num2;
+                            calculations.setText("" + result);
+                        }
+                        case '/' -> {
+                            if (num2 == 0.0) {
+                                calculations.setText("Cannot divide by 0!");
+                            } else {
+                                result = num1 / num2;
+                                calculations.setText("" + result);
+                            }
+                        }
+                    }
+                } else if (Double.toString(num1).isEmpty()) {
+                    if (!calculations.getText().isEmpty()) {
+                        num1 = Double.parseDouble(calculations.getText());
+                    }
+                }
+                if (history.getText().isEmpty()) {
+                    history.append(num1 + " " + operator + " " + num2 + " = " + result);
+                } else {
+                    history.append("\n" + num1 + " " + operator + " " + num2 + " = " + result);
+                }
+            }
+        }
+        if (e.getSource().equals(hstB)) {
+            historyFrame.setVisible(true);
+        }
+        //add a keyboard input check here
     }
 }
